38bb78979c617eb6c2bbdc7386e99ae20fc4cff5
dhirajbendre <75013590+dhirajbendre@users.noreply.github.com>
2021-12-08 17:06:09 +0530

    Add files via upload

---
+++ sleep.py
@@ -0,0 +1,6 @@
+import csv
+
+with open("sleep.csv") as csv_file:
+      df = csv.DictReader(csv_file)
+      fig = px.scatter(df,x="Coffee in ml", y="sleep in hours", color="week")
+      fig.show()
